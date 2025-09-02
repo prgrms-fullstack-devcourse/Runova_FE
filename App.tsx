9415e1f3c0ffa1ee533840b21db0cc8d53dc2266
Monixc <chris13404@gmail.com>
2025-09-02 15:11:01 +0900

    feat: Mapbox 설정 추가 및 액세스 토큰 설정 방식 변경
    - app.json에 Mapbox 관련 설정 추가
    - App.tsx에서 Mapbox 액세스 토큰 설정 방식을 환경 변수에서 expoConfig를 통해 가져오도록 변경

--- App.tsx
+++ App.tsx
@@ -2,11 +2,16 @@ import { ThemeProvider } from '@emotion/react';
 import { NavigationContainer } from '@react-navigation/native';
 import Toast from 'react-native-toast-message';
 import Mapbox from '@rnmapbox/maps';
+import Constants from 'expo-constants';
 
 import RootNavigator from '@/navigation/RootNavigator';
 import { theme } from '@/styles/theme';
 
-Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_TOKEN || '');
+Mapbox.setAccessToken(
+  Constants.expoConfig?.plugins?.find(
+    (plugin) => Array.isArray(plugin) && plugin[0] === '@rnmapbox/maps',
+  )?.[1]?.RNMapboxMapsAccessToken || '',
+);
 
 export default function App() {
   return (
