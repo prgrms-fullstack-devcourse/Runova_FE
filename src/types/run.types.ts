ca8c9d8d4a4fb8a818e24c9469b2021abb69a7f3
Monixc <chris13404@gmail.com>
2025-09-10 22:57:18 +0900

    refactor: Run 관련 타입 일부 정리

--- src/types/run.types.ts
+++ src/types/run.types.ts
@@ -1,34 +1,3 @@
-import type { Feature, LineString } from 'geojson';
-import type * as Location from 'expo-location';
-
-export interface RunState {
-  isLocked: boolean;
-  showExitModal: boolean;
-  startTime: Date | null;
-}
-
-export interface RunMapProps {
-  location: Location.LocationObject;
-  routeGeoJSON: Feature<LineString>;
-  isLocked?: boolean;
-}
-
-export interface StatsContainerProps {
-  stats: {
-    distance: number;
-    calories: number;
-    pace: string;
-  };
-}
-
-export interface ControlContainerProps {
-  isLocked: boolean;
-  isTracking: boolean;
-  onLockPress: () => void;
-  onToggleTracking: () => void;
-  onExitPress: () => void;
-}
-
 export interface RunningRecordRequest {
   path: [number, number][];
   startAt: Date;
