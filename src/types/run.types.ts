b80166ede7e087b8b9e13eac6e0ec7eb7b1d2256
Monixc <chris13404@gmail.com>
2025-09-10 16:23:43 +0900

    feat: 러닝 기록 요청 및 응답 타입 정의 추가

--- src/types/run.types.ts
+++ src/types/run.types.ts
@@ -28,3 +28,21 @@ export interface ControlContainerProps {
   onToggleTracking: () => void;
   onExitPress: () => void;
 }
+
+export interface RunningRecordRequest {
+  path: [number, number][];
+  startAt: Date;
+  endAt: Date;
+  pace: number;
+  calories: number;
+}
+
+export interface RunningRecordResponse {
+  id: number;
+  path: [number, number][];
+  startAt: string;
+  endAt: string;
+  pace: number;
+  calories: number;
+  createdAt: string;
+}
