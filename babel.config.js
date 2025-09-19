79232b7e3a555182ee465070d4699cf6925f6c16
Monixc <chris13404@gmail.com>
2025-09-20 02:05:20 +0900

    chore: babel 설정에서 react-native-reanimated/plugin을 react-native-worklets/plugin으로 변경

--- babel.config.js
+++ babel.config.js
@@ -11,7 +11,7 @@ module.exports = function (api) {
           extensions: ['.ts', '.tsx', '.js', '.json'],
         },
       ],
-      'react-native-reanimated/plugin',
+      'react-native-worklets/plugin',
     ],
   };
 };
